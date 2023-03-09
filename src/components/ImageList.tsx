839e4a3c367b829b6e02880ea7e155ede70ab358
thepoy <thepoy@163.com>
2023-03-09 17:58:16 +0800

    完成删除功能，添加浮动按钮

--- src/components/ImageList.tsx
+++ src/components/ImageList.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Card, List, Button, Tooltip } from 'antd'
+import React, { useEffect, useState } from 'react'
+import { Card, List, Button, Tooltip, FloatButton } from 'antd'
 import { EyeOutlined, DeleteOutlined } from '@ant-design/icons'
 import { useLocation } from 'react-router-dom'
 import { open } from '@tauri-apps/api/shell'
@@ -10,10 +10,14 @@ const { Meta } = Card
 interface ActionsProps {
   show: boolean
   path: string
+  removeImage: (path: string) => void
 }
 
 const Actions: React.FC<ActionsProps> = (props) => {
   // TODO: hover 时显示，离开后隐藏
+
+  useEffect(() => {})
+
   return props.show
     ? (
     <div className="actions">
@@ -30,12 +34,14 @@ const Actions: React.FC<ActionsProps> = (props) => {
           }}
         />
       </Tooltip>
-      <Tooltip
-        placement="left"
-        title="移除此图片"
-        color={'rgba(0, 0, 0, 0.6)'}
-      >
-        <Button type="text" icon={<DeleteOutlined />} />
+      <Tooltip placement="left" title="移除此图片" color={'rgba(0, 0, 0, 0.6)'}>
+        <Button
+          type="text"
+          icon={<DeleteOutlined />}
+          onClick={() => {
+            props.removeImage(props.path)
+          }}
+        />
       </Tooltip>
     </div>
       )
@@ -50,32 +56,47 @@ const ImageList: React.FC = () => {
   const location = useLocation()
   const { state }: { state: State } = location
 
+  const [images, setImages] = useState(state.images)
+
+  const removeImage = (path: string): void => {
+    setImages(images.filter((item) => item.path !== path))
+  }
+
   return (
-    <div id="image-list">
-      <List
-        grid={{
-          xs: 2,
-          sm: 2,
-          md: 3,
-          lg: 4,
-          xl: 5,
-          xxl: 6
-        }}
-        dataSource={state.images}
-        renderItem={(item, idx) => (
-          <List.Item>
-            <Card
-              hoverable
-              style={{ width: 226, height: 319 }}
-              cover={<img alt={item.name} src={item.url} />}
-              extra={<Actions show={true} path={item.path} />}
-            >
-              <Meta title={item.name} />
-            </Card>
-          </List.Item>
-        )}
-      />
-    </div>
+    <>
+      <div id="image-list">
+        <List
+          grid={{
+            xs: 2,
+            sm: 2,
+            md: 3,
+            lg: 4,
+            xl: 5,
+            xxl: 6
+          }}
+          dataSource={images}
+          renderItem={(item) => (
+            <List.Item>
+              <Card
+                hoverable
+                style={{ width: 226, height: 319 }}
+                cover={<img alt={item.name} src={item.url} />}
+                extra={
+                  <Actions
+                    show={true}
+                    path={item.path}
+                    removeImage={removeImage}
+                  />
+                }
+              >
+                <Meta title={item.name} />
+              </Card>
+            </List.Item>
+          )}
+        />
+      </div>
+      <FloatButton tooltip={<div>合并</div>} onClick={() => { console.log('合并为 pdf', images) }}/>
+    </>
   )
 }
 
