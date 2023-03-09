44b3af6e45b1b2f3b3cea5de7771508ec2f414c5
thepoy <thepoy@163.com>
2023-03-09 17:31:08 +0800

    添加按钮

--- src/components/ImageList.tsx
+++ src/components/ImageList.tsx
@@ -1,10 +1,46 @@
 import React from 'react'
-import { Card, List } from 'antd'
+import { Card, List, Button, Tooltip } from 'antd'
+import { EyeOutlined, DeleteOutlined } from '@ant-design/icons'
 import { useLocation } from 'react-router-dom'
 import { open } from '@tauri-apps/api/shell'
 import '~/styles/imageList.scss'
 
 const { Meta } = Card
+
+interface ActionsProps {
+  show: boolean
+  path: string
+}
+
+const Actions: React.FC<ActionsProps> = (props) => {
+  // TODO: hover 时显示，离开后隐藏
+  return props.show
+    ? (
+    <div className="actions">
+      <Tooltip
+        placement="left"
+        title="用图片浏览器打开"
+        color={'rgba(0, 0, 0, 0.6)'}
+      >
+        <Button
+          type="text"
+          icon={<EyeOutlined />}
+          onClick={async () => {
+            await open(props.path)
+          }}
+        />
+      </Tooltip>
+      <Tooltip
+        placement="left"
+        title="移除此图片"
+        color={'rgba(0, 0, 0, 0.6)'}
+      >
+        <Button type="text" icon={<DeleteOutlined />} />
+      </Tooltip>
+    </div>
+      )
+    : null
+}
 
 interface State {
   images: ImageItem[]
@@ -17,22 +53,22 @@ const ImageList: React.FC = () => {
   return (
     <div id="image-list">
       <List
-          grid={{
-            gutter: 16,
-            xs: 2,
-            sm: 2,
-            md: 4,
-            lg: 4,
-            xl: 6,
-            xxl: 3
-          }}
+        grid={{
+          xs: 2,
+          sm: 2,
+          md: 3,
+          lg: 4,
+          xl: 5,
+          xxl: 6
+        }}
         dataSource={state.images}
-        renderItem={(item) => (
+        renderItem={(item, idx) => (
           <List.Item>
             <Card
               hoverable
               style={{ width: 226, height: 319 }}
-              cover={<img alt={item.name} src={item.url} onClick={async () => { await open(item.path) }}/>}
+              cover={<img alt={item.name} src={item.url} />}
+              extra={<Actions show={true} path={item.path} />}
             >
               <Meta title={item.name} />
             </Card>
